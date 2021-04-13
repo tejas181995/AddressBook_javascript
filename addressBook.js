8c16ac1323fe629ea2effecdb144f5d48e59a9ad
Tejas <tejasdev95@gmail.com>
2021-04-13 16:17:13 +0530

    added method to validate user input

--- addressBook.js
+++ addressBook.js
@@ -1,15 +1,41 @@
 const usrInput = require("readline-sync");
-const fields =["firstName", "lastName", "address", "city", "state", "zipCode", "phoneNumber", "email"];
+const fields = ["firstName", "lastName", "address", "city", "state", "zipCode", "phoneNumber", "email"];
+const regexPattern = ["^[A-Z]{1}[a-z]{2,14}$", "^[A-Z]{1}[a-z]{2,14}$", "^[A-Z]{1}[a-z]{3,14}$", "^[A-Z]{1}[a-z]{3,14}$", "^[A-Z]{1}[a-z]{2,14}$", "^[0-9]{6,}", "^[0-9]{2}[ ][6-9]{1}[0-9]{9}$", "^[A-Za-z0-9+-]+(\\.[A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]{2,}){1,2}$"]
 
-var Contact = function(values){
-    this.values = values;
+var Contact = function (values) {
+    this.contactDetails = contactDetails;
+    this.printContact = function() {
+        console.log(contact);
+    }
 }
 
-let values = new Array()
-for(var i = 0; i <= fields.length - 1 ; i++){
-    values[i] = usrInput.question(`Enter ${fields[i]}: `);
+let contactDetails = new Array()
+
+function createContact() {
+    let temp;
+    for (var i = 0; i <= fields.length - 1; i++) {
+        let input = true;
+        while (input) {
+            temp = usrInput.question(`Enter ${fields[i]}: `);
+            if (temp.match(regexPattern[i])) {
+                contactDetails[i] = temp;
+                input = false;
+            }
+            else {
+                try {
+                    throw new Error('Invalid Input Enter a valid input');
+                }
+                catch (error) {
+                    console.log(error);
+                    continue;
+                }
+            }
+
+        }
+    }
 }
 
-let contact = new Contact(values)
+createContact();
 
-console.log(contact);
+let contact = new Contact(contactDetails)
+contact.printContact();
